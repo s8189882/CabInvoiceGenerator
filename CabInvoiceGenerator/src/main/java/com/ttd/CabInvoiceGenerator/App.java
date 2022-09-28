8350edd9cd9c554a0122de5dbb4bc6b4ae23d1e2
s8189882 <s8189882@gmail.com>
2022-09-28 17:25:02 +0530

    create master branch

---
+++ CabInvoiceGenerator/src/main/java/com/ttd/CabInvoiceGenerator/App.java
@@ -0,0 +1,13 @@
+package com.ttd.CabInvoiceGenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
